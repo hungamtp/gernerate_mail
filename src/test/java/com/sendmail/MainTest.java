c15ddd71aa421c7557476a63b9556cc45415d18a
Hung <hungamtp@gmail.com>
2022-07-02 19:02:09 +0700

    add case success

--- src/test/java/com/sendmail/MainTest.java
+++ src/test/java/com/sendmail/MainTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.sendmail.Main.generateEmail;
 
@@ -18,7 +17,7 @@ public class MainTest extends TestCase {
     private final static String OUTPUT_FOLDER = "src/test/output";
     private final static String ERROR_FILE = "src/test/errors/errors.csv";
 
-    private final static String TEMPLATE_FILE_WRONG = "src/test/input/email_template_wrong.json";
+    private final static String TEMPLATE_FILE_WRONG = "src/test/input/email_template_wrong.json.json";
 
     @Test
     public void testGenerateEmailSuccess() throws IOException, ParseException {
@@ -27,33 +26,29 @@ public class MainTest extends TestCase {
         File outputFolder = new File(OUTPUT_FOLDER);
         File errorFile = new File(ERROR_FILE);
         generateEmail(outputFolder, errorFile, customerFile, templateFile);
-        assertEquals(outputFolder.list().length, 4);
-    }
 
-    @Test
-    public void testCheckReadCustomerFileDataSuccess() throws IOException, ParseException {
-        File customerFile = new File(CUSTOMER_FILE);
-        List<Customer> customers = new ArrayList<>();
+
+        List<String> emails = new ArrayList<>();
         try {
             Reader reader = new FileReader(customerFile);
             for (var customer : readAll(reader)) {
                 if (customer.length > 0) {
                     if (customer.length == 4)
-                        customers.add(new Customer(customer[0], customer[1], customer[2], customer[3], true));
-                    else
-                        customers.add(new Customer(customer[0], customer[1], customer[2], "", false));
+                        emails.add( customer[3] );
                 }
             }
             // remove header
-            customers.remove(0);
+            emails.remove(0);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        assertEquals(customers.size(), 9);
-        assertEquals(customers.stream().filter(customer -> customer.getHasMail()).collect(Collectors.toList()).size(), 5);
-        assertEquals(customers.stream().filter(customer -> !customer.getHasMail()).collect(Collectors.toList()).size(), 4);
+        assertEquals(outputFolder.list().length, emails.size());
 
+        for(var email : emails){
+            File file = new File(OUTPUT_FOLDER + "/" + email +".json");
+            assertTrue(file.exists());
+        }
     }
 
     private static List<String[]> readAll(Reader reader) throws Exception {
@@ -67,15 +62,11 @@ public class MainTest extends TestCase {
 
     @Test(expected = FileNotFoundException.class)
     public void testGenerateEmailWrongInputFile() throws IOException, ParseException {
-        try {
             File templateFile = new File(TEMPLATE_FILE_WRONG);
             File customerFile = new File(CUSTOMER_FILE);
             File outputFolder = new File(OUTPUT_FOLDER);
             File errorFile = new File(ERROR_FILE);
             generateEmail(outputFolder, errorFile, customerFile, templateFile);
-        } catch (FileNotFoundException e) {
-            assertTrue(e.getMessage().contains(TEMPLATE_FILE_WRONG) || e.getMessage().contains("not found"));
-        }
     }
 
 
